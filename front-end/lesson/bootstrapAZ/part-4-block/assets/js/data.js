d42a73668aabf8a85f90bc944538e361175c1e18
Khoa Tran <lamvankhoat1@gmail.com>
2024-04-22 11:42:43 +0700

    update

---
+++ front-end/lesson/bootstrapAZ/part-4-block/assets/js/data.js
@@ -0,0 +1,75 @@
+let list_product;
+list_product = [
+    {
+        img: 'assets/images/product-1.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '800.000đ',
+        newPrice: '500.000đ',
+    },
+    {
+        img: 'assets/images/product-2.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '700.000đ',
+        newPrice: '500.000đ',
+    },
+    {
+        img: 'assets/images/product-3.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '799.000đ',
+        newPrice: '400.000đ',
+    },
+    {
+        img: 'assets/images/product-4.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '650.000đ',
+        newPrice: '380.000đ',
+    },
+    {
+        img: 'assets/images/product-3.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '123.000đ',
+        newPrice: '110.000đ',
+    },
+    {
+        img: 'assets/images/product-2.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '243.000đ',
+        newPrice: '210.000đ',
+    },
+    {
+        img: 'assets/images/product-1.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '563.000đ',
+        newPrice: '435.000đ',
+    },
+    {
+        img: 'assets/images/product-4.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '651.000đ',
+        newPrice: '500.000đ',
+    },
+    {
+        img: 'assets/images/product-2.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '424.000đ',
+        newPrice: '100.000đ',
+    },
+    {
+        img: 'assets/images/product-3.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '655.000đ',
+        newPrice: '234.000đ',
+    },
+    {
+        img: 'assets/images/product-4.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '563.000đ',
+        newPrice: '122.000đ',
+    },
+    {
+        img: 'assets/images/product-1.jpg',
+        name: 'Lorem ipsum dolor sit amet.',
+        oldPrice: '739.000đ',
+        newPrice: '300.000đ'
+    }
+]
